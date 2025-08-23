7a2d94147d9ea5e2d50c2f3aed4570ad53d2a97c
Elias Lima Leite <limaleiteelias99@gmail.com>
2025-08-23 15:58:20 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+const alunos = [
+    {nome: "Ana", nota: 10},
+    {nome: "Pedro", nota: 5},
+    {nome: "João", nota: 9},
+    {nome: "Maria", nota: 7}
+]
+
+function bestOfTheClass(alunos){
+    let result = alunos.filter(aluno => aluno.nota >= 6).map(aluno => aluno.nome);
+    return `Os melhores alunos foram ${result.join(", ")}.`;
+}
+
+const result = bestOfTheClass(alunos);
+console.log(result);
